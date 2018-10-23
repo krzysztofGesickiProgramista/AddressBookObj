e1e45ca19cef7fae990f910c2ef6dd9dc39822e0
gesik92 <krzysztof.gesicki.programista@gmail.com>
2018-10-23 19:37:55 +0200

    Add registered user to vector

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "Uzytkownik.h"
+#include <windows.h>
 
 using namespace std;
 
@@ -11,8 +12,13 @@ class KsiazkaAdresowa
     int idUsunietegoAdresata;
     vector <Uzytkownik> uzytkownicy;
 
+    Uzytkownik podajDaneNowegoUzytkownika();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
+
 public:
     void rejestracjaUzytkownika();
     void logowanieUzytkownika();
+    void wypiszWszystkichUzytkownikow();
 
 };
--- main.cpp
+++ main.cpp
@@ -6,5 +6,11 @@ using namespace std;
 int main()
 {
     KsiazkaAdresowa ksiazkaAdresowa;
+
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+
     return 0;
 }
