d2fcbb0cbb46634e07a2f2da0adeaf0553484aac
kgesicki <gesicki.krzysztof@gmail.com>
2018-10-27 20:20:12 +0200

    added global and local variables and FEATURE_SETUP-FILENAME-AT-BEGINING

--- PlikZUzytkownikami.h
+++ PlikZUzytkownikami.h
@@ -13,15 +13,14 @@ using namespace std;
 
 class PlikZUzytkownikami
 {
-    string nazwaPlikuZUzytkownikami;
+    const string nazwaPlikuZUzytkownikami;
     bool czyPlikJestPusty();
-    fstream plikTekstowy;
 
     string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
     Uzytkownik pobierzDaneUzytkownika(string daneJednegoUzytkownikaOddzielonePionowymiKreskami);
 
 public:
-    PlikZUzytkownikami();
+    PlikZUzytkownikami(string NAZWAPLIKUZUZYTKOWNIKAMI) : nazwaPlikuZUzytkownikami(NAZWAPLIKUZUZYTKOWNIKAMI){};
     void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
     vector <Uzytkownik> wczytajUzytkownikowZPliku();
 
